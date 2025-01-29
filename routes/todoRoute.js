4e15066cfba96e7fae59e141a709b858ef730718
SNayan12SmrtGen <sakshi.nayan@smartgenesis.io>
2025-01-29 23:36:57 +0530

    changes

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/", authMiddleware, createTodo);
 router.get("/", authMiddleware, getTodos);
-router.put('/:id', authenticate, updateTodo);
-router.delete('/:id', authenticate, deleteTodo);
+router.put('/:id', authMiddleware, updateTodo);
+router.delete('/:id', authMiddleware, deleteTodo);
 
 module.exports = router;
