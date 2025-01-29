97f55f2406f0c3a4b75e51d96de34b6f2e12a2ae
SNayan12SmrtGen <sakshi.nayan@smartgenesis.io>
2025-01-30 00:16:51 +0530

    server run msg

--- server.js
+++ server.js
@@ -14,5 +14,9 @@ app.use(bodyParser.json());
 app.use("/auth", authRoutes);
 app.use("/todos", todoRoutes);
 
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is running successfully!" });
+  });
+  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
